a1e73d0f2b1b94df4cb69733f1201ea4f9dae435
Biokes <106444765+Biokes@users.noreply.github.com>
2025-01-20 03:29:31 +0100

    complete progress bar

--- src/components/layouts/progress.tsx
+++ src/components/layouts/progress.tsx
@@ -1,14 +1,18 @@
-import {CircularProgressbar} from "react-circular-progressbar";
+import {buildStyles, CircularProgressbar} from "react-circular-progressbar";
 import React from "react";
 import ColoredDot from "./coloredDot";
 
-export default function Progress(props:{percent:number,text:string, color:string}){
+type RGBA = `rgba(${number}, ${number}, ${number}, ${number})`;
+type HSLA = `hsla(${number}, ${number}%, ${number}%, ${number})`;
+type Color = string | RGBA | HSLA;
+export default function Progress(props:{percent:number,text:string, color?:Color}){
     return (
-        <div className={'w-[70px] h-[70px] md:w-[120px] md:h-[120px]'}>
-            <CircularProgressbar value={props.percent} text={`${props.percent}%`} strokeWidth={10} />
+        <div className={'w-[70px] h-[70px]'}>
+            <CircularProgressbar value={props.percent} text={`${props.percent}%`} strokeWidth={10}
+                                 styles={buildStyles({pathColor: props.color || '#d6d6d6'})}/>
             <div className={'flex gap-[10px] items-center justify-center'}>
-                <ColoredDot color={props.color} size={10}/>
-                <p className={'capitalize text-[11px] md:text-[15px] font-[650] text-nowrap mt-[5px]'}>{props.text}</p>
+                <ColoredDot color={props.color as string} size={10}/>
+                <p className={'capitalize text-[11px]  font-[650] text-nowrap mt-[5px]'}>{props.text}</p>
             </div>
         </div>
     )
